8e939a80a4d5a469955693fae1ede3e10592c7b2
Callum Priestman <callum.priestman@hotmail.co.uk>
2024-07-25 11:24:29 +0000

    backup for migration error testing

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,7 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
     class Meta:
         ordering = ["-created_on"]
     
@@ -32,6 +33,8 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+
     class Meta:
         ordering = ["created_on"]
     
